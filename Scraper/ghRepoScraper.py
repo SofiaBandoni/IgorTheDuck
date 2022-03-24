bae88ee4a37c1df88231e28dd22bdc5096e3b1a5
SofiaBandoni <sofiabandoni@gmail.com>
2022-03-24 00:39:56 -0300

    Continue developing scraper

--- Scraper/ghRepoScraper.py
+++ Scraper/ghRepoScraper.py
@@ -1,21 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_repositories():
-    url = 'https://github.com/SofiaBandoni/IgorTheDuck'
+# Indicar la url del repositorio a escrapear
+github_url = 'https://github.com/SofiaBandoni/IgorTheDuck'
+
+# Armo una funcion que recorre el repositorio y va a devolver los links de cada carpeta dentro del mismo
+def get_repositories(github_url):
+    url = github_url
     response = requests.get(url)
     response_code = response.status_code
     if response_code != 200:
-        print("Error ocurred")
+        print("Error ocurred: " + str(response.status_code))
         return
     html_content = response.content
     dom = BeautifulSoup(html_content, 'html.parser')
     all_folders = dom.find_all(class_ = 'js-navigation-open Link--primary')
+    links = []
     for folder in range(len(all_folders)):
-        print(all_folders[folder].get("href"))
+        links.append("https://github.com" + all_folders[folder].get("href"))       
+    return(links)
+
+          
+def get_dagfiles(repos):
+    for repo in repos:
+        url = repo
+        response = requests.get(url)
+        response_code = response.status_code
+        if response_code != 200:
+            print("Error ocurred: " + str(response.status_code))
+            return
+        html_content = response.content
+        dom = BeautifulSoup(html_content, 'html.parser')
+        dagfiles = dom.find_all(class_ = 'js-navigation-open Link--primary')
+        #print(dagfiles)
+        dagfiles_links = []
+        for dagfile in range(len(dagfiles)):
+            dagfiles_links.append("https://github.com" + dagfiles[dagfile].get("href"))
+        print(dagfiles_links)
+        #return(dagfiles_links)
+        
     
+
 if __name__ == '__main__':
-    print('Started Scraping')
-    get_repositories()
-    
+    print('Started Scraping Main Repository')
+    repos = get_repositories(github_url)
+    print("These are the folders links in main repository:" )
+    print(repos)
+    print("Started Scraping Folders In Main Repository")
+    print("These are the links in folders of main repository: ")
+    dagfiles = get_dagfiles(repos)
+    #print("These are de dagfiles links:")
+    #print(dagfiles)
     
