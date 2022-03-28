ef10edf214712b83733bdaa14bc85abe8c481cb0
SofiaBandoni <sofiabandoni@gmail.com>
2022-03-28 00:22:38 -0300

    Add get_sql_tables function

--- Scraper/codeScraperv2.py
+++ Scraper/codeScraperv2.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from sql_metadata import Parser
 
 # Indicar la url del repositorio a escrapear
 github_url = 'https://raw.githubusercontent.com/SofiaBandoni/IgorTheDuck/master/Folder1/query1.hql'
 
-# Armo una funcion que recorre el repositorio y va a devolver los links de cada carpeta dentro del mismo
-def get_code(github_url):
+
+def get_raw_code(github_url):
     url = github_url
     response = requests.get(url)
     response_code = response.status_code
@@ -14,8 +15,15 @@ def get_code(github_url):
         return
     html_content = response.content
     dom = BeautifulSoup(html_content, 'html.parser')
-    print(dom)
+    return(dom)
     
+def get_sql_tables(raw_code):
+    tables = Parser(str(raw_code)).tables
+    return(tables)
+
 if __name__ == '__main__':
-    get_code(github_url)
+    raw_code = get_raw_code(github_url)
+    print(raw_code)
+    sql_tables = get_sql_tables(raw_code)
+    print(sql_tables)
     
