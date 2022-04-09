f086d6fa8b1b041ee97615aaeadf30242fa39ef6
SofiaBandoni <sofiabandoni@gmail.com>
2022-04-09 18:02:58 -0300

    Add Partition identifier

--- Scraper/sqlParser.py
+++ Scraper/sqlParser.py
@@ -1,67 +1,17 @@
 from sql_metadata import Parser
 import sqlparse
 
-raw_sql = """SET hive.exec.dynamic.partition=true;
-
-SET hive.exec.dynamic.partition.mode=nonstrict;
+raw_sql = """drop table if exists data_lake_analytics.tmp_abnbankinfo{{ envCode }} purge;
 
  
 
-REFRESH TABLE data_lake_analytics.tmp_abnpaychannel{{ envCode }};
+create external table data_lake_analytics.tmp_abnbankinfo{{ envCode }} as
 
- 
+select distinct bank_code,
 
-insert overwrite table data_lake_analytics.STG_ABNpagos_m{{ envCode }} partition(periodo, flag_fan)
+    FIRST_value(bank_name) over(partition by bank_code order by day desc) as bank_name
 
-SELECT
-
-    NVL(pri_identity, c.linea) as linea,
-
-    payment_method_id,
-
-    bank_code,
-
-    p.medio_pago,
-
-    cp.description as tipo_pago,
-
-    p.canal_pago,
-
-    (p.payment_amt/1000000) as importe,
-
-    cast(PAYMENT_DATE as int) as fecha,
-
-    cast(substr(PAYMENT_DATE,1,6) as int) AS periodo,
-
-    1 as flag_fan
-
-from data_lake_analytics.tmp_abnpagosfan{{ envCode }} p
-
- 
-
-left join (select contrato_key, linea, cbs_cust_id
-
-            from data_lake_analytics.STG_ABNparque_m{{ envCode }}
-
-            where periodo = from_unixtime(unix_timestamp(date_add(cast('{{ next_ds }}' as date),-day(cast('{{ next_ds }}' as date))),'yyyy-MM-dd'), 'yyyyMM')
-
-    ) c
-
-    on c.cbs_cust_id = p.cust_id
-
- 
-
-left join data_lake_analytics.tmp_abnpaychannel{{ envCode }} cp
-
-    on cp.channel_id = p.channel_id
-
-where substr(PAYMENT_DATE,1,6) = from_unixtime(unix_timestamp(date_add(cast('{{ next_ds }}' as date),-day(cast('{{ next_ds }}' as date))),'yyyy-MM-dd'), 'yyyyMM')
-
-;
-
- 
-
-REFRESH TABLE data_lake_analytics.STG_ABNpagos_m{{ envCode }};"""
+from pre_facturacion.cbs_ar_dic_bank_info;"""
 
 splitted = sqlparse.split(raw_sql)
 
@@ -73,13 +23,15 @@ for query in splitted:
         print("Se descarta")
 
 
-sql = to_query_parser.replace("overwrite","").replace("partition", "").replace("create external table", "insert into")
+sql = to_query_parser.replace("overwrite","").replace("create external table", "insert into")
 tablas = Parser(str(sql)).tables
 
 for tabla in tablas:
     sink = sql.find(tabla)
     determine_sink = sql[0:sink]
-    if "insert" in determine_sink and "from" not in determine_sink:
+    if "insert" in determine_sink and "partition" not in determine_sink and "from" not in determine_sink:
         print(tabla + ": sink")
+    elif "partition" in determine_sink and "from" not in determine_sink:
+        print(tabla + ": partition")
     else:
         print(tabla + ": source")
